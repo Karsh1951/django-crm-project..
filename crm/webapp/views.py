98af60c0ef4e82d8532a5c4cd220bd2ea68de920
Karsh1951 <giftonkarsh9851@gmail.com>
2024-05-20 18:06:33 +0300

    i implemented the views:the register view and the log in view and connected the front end to the back end

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,70 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm,LoginForm
 
-# Create your views here.
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+
+from .models import Record
+
+# HomePage.
 def home(request):
     return render(request, 'webapp/index.html')
+
+#Register a user
+def register(request):
+    form = CreateUserForm()
+
+    if request.method=='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('my-login')
+
+
+    context= {'form':form}
+
+    return render(request,'webapp/register.html',context=context)
+
+
+#loging in a user
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request,data=request.POST)
+
+        if form.is_valid():
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+
+
+            user= authenticate(request,username=username,password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect('dashboard')
+
+    context= {'form':form}
+    return render (request, 'webapp/my-login.html', context=context)
+
+
+#dashboard view
+@login_required(login_url='my-login')
+def dashboard(request):
+
+    my_records = Record.objects.all()
+
+    context = {'records':my_records}
+
+    return render(request, 'webapp/dashboard.html' , context=context)
+
+
+
+
+
+def user_logout(request):
+    auth.logout(request)
+
+    return redirect('my-login')
